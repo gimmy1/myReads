050cb78df73f6af4b1ba723b74f770aad96fa4a4
gali7221 <gali7221@gmail.com>
2017-09-13 13:22:48 -0400

    Fix react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Route, Link } from 'react-router-dom'
 import './App.css'
 import * as BooksAPI from './BooksAPI'
 import BookShelf from './BookShelf'
+import BookSearch from './BookSearch'
 
 class BooksApp extends React.Component {
   state = {
     books: [],
+    view: 'notsearch'
   }
 
   // Retrieve all books from API
@@ -19,22 +22,37 @@ class BooksApp extends React.Component {
   }
   componentDidMount = () => this.getAllBooks()
 
-  // componentDidMount() {
-  //   BooksAPI.getAll().then((books) => {
-  //     this.setState({ books })
-  //   })
-  // }
 
   moveShelf = (book, newShelf) => {
     BooksAPI.update(book, newShelf).then(() => this.getAllBooks())
   }
 
+
   render() {
     return (
-      <BookShelf
-        books={this.state.books}
-        moveShelf={this.moveShelf}
-      />
+      <div className="app">
+        <Route path="/search" render={( {history}) => (
+          <BookSearch
+            books={this.state.books}
+            moveShelf={this.moveShelf}
+          />
+        )} />
+
+        <Route exact path="/" render={() => (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>My Reads</h1>
+            </div>
+            <BookShelf
+              books={this.state.books}
+              moveShelf={this.moveShelf}
+            />
+            <div className="open-search">
+              <Link to="/search">Search</Link>
+            </div>
+          </div>
+        )} />
+      </div>
     )
   }
 }
--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,69 +1,20 @@
 import React, { Component} from 'react'
 import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import * as BooksAPI from './BooksAPI'
 import BookList from './BookList'
 
 class BookSearch extends Component {
-  static propTypes = {
-    moveShelf: PropTypes.func.isRequired,
-    isOnShelf: PropTypes.func.isRequired
-  }
 
-  state = {
-    results: [],
-  }
-
-  clearResults = () => (
-    this.setState({results: []})
-  )
-
-  bookSearch = (query) => {
-    BooksAPI.search(query).then(
-      results => {
-        // Result exists and it's not an error
-        if (results && (!results.error)) {
-          for (const book of results) {
-            const b = this.props.isOnShelf(book)
-            book.shelf = b ? b.shelf : 'none'
-          }
-          this.setState({ results })
-        }
-      }
-    )
-
-  }
   render() {
-    const { moveShelf } = this.props
-    const { results } = this.state
     return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link
-            to='/'
-            className="close-search"
-            onClick={this.clearResults}>
-            Close
-          </Link>
-          <div className="search-books-input-wrapper">
-            <input
-              type="text"
-              placeholder="Search by title or author"
-              onChange={e => this.bookSearch(e.target.value)}
-              autoFocus
-            />
-          </div>
-        </div>
-        <div className="search-books-results">
-          <BookList
-            books={results}
-            moveShelf={moveShelf}
-          />
-        </div>
-      </div>
+      <div>Search</div>
     )
   }
 }
 
+// BookSearch.PropTypes = {
+//   moveShelf: PropTypes.func.isRequired,
+// }
 
 export default BookSearch
